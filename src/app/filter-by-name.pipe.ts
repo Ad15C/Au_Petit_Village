a8b38457e9f60e5b1cbf2a9c7ff66ec9e2e9b6a2
adeline <ad15canon@gmail.com>
2026-03-12 11:28:24 +0100

    filter_updates

--- src/app/filter-by-name.pipe.ts
+++ src/app/filter-by-name.pipe.ts
@@ -6,22 +6,18 @@ import { Product } from './product.model';
 })
 export class FilterByNamePipe implements PipeTransform {
 
-  transform(products: Product[], filterBy: string): Product[] {
-    console.log('Produits avant filtrage:', products);
-    console.log('Filtre appliqué:', filterBy);
-    
+  transform(products: Product[], searchTerm: string): Product[] {
+
     if (!products || products.length === 0) {
       return [];
     }
-  
-    if (filterBy.toLowerCase() === 'all') {
+
+    if (!searchTerm || searchTerm.trim() === '') {
       return products;
-    } else if (filterBy.toLowerCase() === 'name') {
-      return products.sort((a, b) => a.name.localeCompare(b.name));
-    } else {
-      return products.filter((prd) =>
-        prd.name.toLowerCase().includes(filterBy.toLowerCase())
-      );
     }
+
+    return products.filter((prd) =>
+      prd.name.toLowerCase().includes(searchTerm.trim().toLowerCase())
+    );
   }
 }
